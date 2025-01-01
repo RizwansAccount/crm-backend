c0fb95dbd25706354f056a39f240ef4d5496ed86
Rizsid <rizsid16@gmail.com>
2025-01-01 16:55:59 +0500

    chnages

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -5,9 +5,9 @@ import { NoteValidation } from '../validations/note.validation.js';
 
 const router = epxress.Router();
 
-router.get('/', NoteController.getAll);
+router.get('/', validate(NoteValidation.get.querySchema, 'query'), NoteController.getAll);
 
-router.get('/:id', NoteController.getById);
+router.get('/:id', validate(NoteValidation.get.querySchema, 'query'), NoteController.getById);
 
 router.post('/', validate(NoteValidation.create.bodySchema), NoteController.create);
 
--- src/validations/note.validation.js
+++ src/validations/note.validation.js
@@ -2,6 +2,13 @@ import Joi from "joi";
 
 export const NoteValidation = {
 
+    get: {
+        querySchema: Joi.object().keys({
+            source: Joi.string().required(),
+            source_id: Joi.string().required(),
+        })
+    },
+
     create: {
         bodySchema: Joi.object().keys({
             note: Joi.string().required(),
